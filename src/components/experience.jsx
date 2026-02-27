520f7bd8fb4c8044c730fa6c914e6eb9beee59ff
silverwolf1223 <101886723+silverwolf1223@users.noreply.github.com>
2026-02-26 23:39:59 -0600

    adding and deleting work experiences

---
+++ src/components/experience.jsx
@@ -0,0 +1,23 @@
+
+
+export default function Experience(props){
+    return <div key={props.elem.id}>
+        <div>
+            <label>Job Title: </label>
+            <input name="jobTitle" data-index={props.index} onChange={props.handleExperience} value={props.elem.jobTitle}></input>
+            <label>Employer: </label>
+            <input name="employer" data-index={props.index} onChange={props.handleExperience} value={props.elem.employer}></input>
+        </div>
+        <div>
+            <label>Start Date: </label>
+            <input type="date" name="startDate" data-index={props.index} onChange={props.handleExperience} value={props.elem.startDate}></input>
+            <label>End Date: </label>
+            <input type="date" name="endDate" data-index={props.index} onChange={props.handleExperience} value={props.elem.endDate}></input>
+        </div>
+        <div>
+            <label>General Description: </label>
+            <textarea name="description" data-index={props.index} onChange={props.handleExperience} value={props.elem.description}></textarea>
+        </div>
+        <button type="button" onClick={() => props.deleteExperience(props.index)}>delete</button>
+    </div>
+}
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import "../form.css";
-import Education from "./education.jsx"
+import Education from "./education.jsx";
+import Experience from "./experience.jsx";
 
 function Form(){
     const [formState, setFormState] = useState({
@@ -69,7 +70,7 @@ function Form(){
         setFormState(newState);
     }
 
-    function deleteExperiences(index){
+    function deleteExperience(index){
         let experiences = formState.experience;
         experiences.splice(index, 1);
         const newState = {
@@ -106,6 +107,20 @@ function Form(){
         })
     }
 
+    function handleExperience(e){
+        const { name, value, } = e.target;
+        const index = e.target.dataset.index;
+        let newExperience = formState.experience;
+        newExperience[index] = {...newExperience[index],
+            [name]: value,
+        }
+        setFormState({
+            generalInfo: formState.generalInfo,
+            education: formState.education,
+            experience: newExperience
+        })
+    }
+
     return <>
     <form>
         <fieldset>Contact Information
@@ -124,27 +139,13 @@ function Form(){
         </fieldset>
         <fieldset>
             Education
-            <button type="button" onClick={addEducation}>add education</button>
-            {formState.education.map((elem, i) => {return <Education index={i} elem={elem} handleEducation={handleEducation} deleteEducation={deleteEducation}/>})}
+            <button type="button" onClick={addEducation}>Add Education</button>
+            {formState.education.map((elem, i) => <Education index={i} elem={elem} handleEducation={handleEducation} deleteEducation={deleteEducation}/>)}
         </fieldset>
         <fieldset>
             Work Experience
-            <div>
-                <label>Job Title: </label>
-                <input name="jobTitle"></input>
-                <label>Employer's name: </label>
-                <input name="employerName"></input>
-            </div>
-            <div>
-                <label>Description: </label>
-                <input type="text"></input>
-            </div>
-            <div>
-                <label>Start Date: </label>
-                <input name="startDate"></input>
-                <label>End Date: </label>
-                <input name="endDate"></input>
-            </div>
+            <button type="button" onClick={addExperience}>Add Experience</button>
+            {formState.experience.map((elem, i) => <Experience index={i} elem={elem} handleExperience={handleExperience} deleteExperience={deleteExperience}/>)}
         </fieldset>
     </form>
 </>
